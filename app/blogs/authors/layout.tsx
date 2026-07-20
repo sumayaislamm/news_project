7d3b698b4d31cf9a3caa3b66da3389d886d9890e
Sumaya <sheikhsumaya622@gmail.com>
2026-07-20 20:57:40 +0600

    author layouts

---
+++ app/blogs/authors/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthorLayout = ({children} : {children : React.ReactNode}) => {
+  return (
+    <div>AuthorLayout
+        {children}
+    </div>
+
+  )
+}
+
+export default AuthorLayout
