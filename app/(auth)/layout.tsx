6f87023d8dd6b7d14f815ab3cecac9ca861a46ff
Sumaya <sheikhsumaya622@gmail.com>
2026-07-21 18:51:27 +0600

    route grouping

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      AuthLayout
+      <br />
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
